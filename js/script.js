cd6f1a8660f636f0ac3e061ab7f4ffdcbc7b9b6a
Daria <wwdarcool@yandex.ru>
2021-12-09 05:57:09 +0300

    add: lesson9

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const title = document.getElementsByTagName('h1')[0];
+const button = document.getElementsByClassName('handler_btn');
+const plus = document.querySelector('.screen-btn');
+const otherPercent = document.querySelectorAll('.other-items.percent');
+const otherNumber = document.querySelectorAll('.other-items.number');
+const inputRange = document.querySelector('.rollback > div > input');
+const span = document.querySelector('.rollback > div > span');
+const totalInput = document.getElementsByClassName('total-input');
+let screen = document.querySelectorAll('.screen');
+
+console.log(totalInput);
