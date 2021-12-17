a02143d02acad6e8a12d29ee089d8ebfeab642de
Daria <wwdarcool@yandex.ru>
2021-12-17 12:00:04 +0300

    fix

--- js/script.js
+++ js/script.js
@@ -5,7 +5,10 @@ const otherPercent = document.querySelectorAll('.other-items.percent');
 const otherNumber = document.querySelectorAll('.other-items.number');
 const inputRange = document.querySelector('.rollback > div > input');
 const span = document.querySelector('.rollback > div > span');
-const totalInput = document.getElementsByClassName('total-input');
+const totalInput = document.getElementsByClassName('total-input')[0];
+const totalInput1 = document.getElementsByClassName('total-input')[1];
+const totalInput2 = document.getElementsByClassName('total-input')[2];
+const totalInput3 = document.getElementsByClassName('total-input')[3];
+const totalInput4 = document.getElementsByClassName('total-input')[4];
 let screen = document.querySelectorAll('.screen');
 
-console.log(totalInput);
