e4c02eb69917a515b1210074df5b390ee51052e6
Daria <wwdarcool@yandex.ru>
2021-12-17 13:32:53 +0300

    add: fix

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
-/ for (let i = 0; i < totalInput.length; i++) {
-//     console.log(totalInput[i]);
-// }
-
-console.log(totalInput);
-console.log(totalInput2);
-console.log(totalInput3);
-
-
-let screenAll = document.querySelectorAll('.screen');
+const title = document.getElementsByTagName('h1')[0];
+const button = document.getElementsByClassName('handler_btn');
+const plus = document.querySelector('.screen-btn');
+const otherPercent = document.querySelectorAll('.other-items.percent');
+const otherNumber = document.querySelectorAll('.other-items.number');
+const inputRange = document.querySelector('.rollback > div > input');
+const span = document.querySelector('.rollback > div > span');
+const totalInput = document.getElementsByClassName('total-input')[0];
+const totalInput1 = document.getElementsByClassName('total-input')[1];
+const totalInput2 = document.getElementsByClassName('total-input')[2];
+const totalInput3 = document.getElementsByClassName('total-input')[3];
+const totalInput4 = document.getElementsByClassName('total-input')[4];
+let screen = document.querySelectorAll('.screen');
 
 
 const appData = {
