982242efc1525f25d9e4e27085fcfba53f4e88e0
Daria <wwdarcool@yandex.ru>
2022-03-04 14:01:24 +0300

    add

--- k.js
+++ k.js
@@ -1,13 +1,21 @@
-const f = [{
+const f = {
     a: 'sdds',
     s: 0,
     sf: 5454
-}];
+};
 
-const d = f.map(item => {
-    item = Object.entries(item).flat()
-    const screen = item.find(i => {
-        return i > 0
-    })
-    //console.log(screen)
+Object.defineProperty(f, 'full', {
+    get() {
+        return `${this.a}`
+    },
+    set(value) {
+        this.cm = value
+    }
 })
+
+f.a = 78
+
+console.log(f.full)
+
+
+
