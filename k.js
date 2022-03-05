569ea03a099197c09739aebf8369722e5a2afee5
Daria <wwdarcool@yandex.ru>
2022-03-05 15:44:15 +0300

    add

--- k.js
+++ k.js
@@ -17,29 +17,79 @@
 
 // console.log(f.full)
 
-'use strict'
+// 'use strict'
 
-const person = {
-    name: 'Daria',
-    age: 22,
-    methed: function() {
-        const arr = [1,2,3];
-        const ars = arr.reduce((acc, item) => {
-            console.log(this)
-            return acc + item
-        }, 0)
-        console.log(this)
-        console.log(ars)
-    }
+// const person = {
+//     name: 'Daria',
+//     age: 22,
+//     methed: function() {
+//         const arr = [1,2,3];
+//         const ars = arr.reduce((acc, item) => {
+//             console.log(this)
+//             return acc + item
+//         }, 0)
+//         console.log(this)
+//         console.log(ars)
+//     }
+// }
+
+// person.methed()
+
+
+// function name() {
+//     console.log(this.name)
+// }
+
+// name.apply(person)
+
+// const arr1 = new Array(1, 2);
+// const arr2 = Array(1, 2)
+// const arr3 = [1, 2]
+
+// console.log(arr3.__proto__)
+// console.log(Array.prototype)
+
+// const human = {
+//     legs: true,
+//     tail: false,
+//     eyes: 'green',
+//     age: 22,
+//     walk() {
+//         console.log('Я хожу')
+//     },
+//     say() {
+//         console.log(this.name)
+//     },
+// }
+
+// const man = Object.create(human);
+
+// const alex = Object.create(man);
+// alex.name = "Alex"
+
+// alex.say()
+
+const Person = function(name) {
+    this.names = name;
+};
+
+const Alex = new Person('Alex')
+
+Person.prototype.say = function() {
+    console.log('Привет')
 }
 
-person.methed()
+const Daria = new Person('Daria')
+Daria.say()
+Alex.__proto__.say()
+console.log(Alex)
 
 
-function name() {
-    console.log(this.name)
-}
 
-name.apply(person)
+
+
+
+
+
 
 
