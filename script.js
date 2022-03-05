2000371bcb5a5f15bdb37f276799cb563d563ab3
Daria <wwdarcool@yandex.ru>
2022-03-05 17:04:03 +0300

    fix: name selector

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const DomElement = function(selector, height, width, bg, fontSize) {
     if (this.selector[0] === '#') {
         const p = document.createElement('p');
         this.DomElement = function() {
-            p.id = selector;
+            p.id = selector.slice(1);
             p.style.cssText=`
             background-color: ${bg};
             width: ${width};
@@ -18,14 +18,13 @@ const DomElement = function(selector, height, width, bg, fontSize) {
             `;
             p.textContent = 'Получилось с #'
             document.querySelector('.container').append(p);
-            console.log(document.querySelector('.container'))
         }()
     } 
     
     if (this.selector[0] === '.') {
         const div = document.createElement('div');
         this.DomElement = function() {
-            div.classList.add(selector);
+            div.classList.add(selector.slice(1));
             div.style.cssText=`
             background-color: ${bg};
             width: ${width};
@@ -34,13 +33,12 @@ const DomElement = function(selector, height, width, bg, fontSize) {
             `;
             div.textContent = 'Получилось'
             document.querySelector('.container').append(div);
-            console.log(document.querySelector('.container'))
         }()
     } 
 }
 
 
 const elem1 = new DomElement('.best', '50px', '50px', 'red', '19px');
-const elem2 = new DomElement('#best', '50px', '50px', 'green', '19px');
+const elem2 = new DomElement('#add', '50px', '50px', 'green', '19px');
 
 
